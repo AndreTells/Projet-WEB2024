3f8237a6fc068fea9214fcd1cc2806dbafdf39c9
hdufaure <145564110+hdufaure@users.noreply.github.com>
2024-06-20 16:57:38 +0200

    creation de compte.php

--- Code/index.php
+++ Code/index.php
@@ -40,6 +40,10 @@ Les formulaires de toutes les vues générées enverront leurs données vers la 
             include("templates/contact.php");
             break;
 
+        case "compte":
+            include("templates/compte.php");
+            break;
+
 
 		default : // si le template correspondant à l'argument existe, on l'affiche
 			if (file_exists("templates/$view.php"))
