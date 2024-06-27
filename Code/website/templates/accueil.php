eb299f728240efb2ea854fb4d37f8782c000ae4f
hdufaure <145564110+hdufaure@users.noreply.github.com>
2024-06-27 10:23:00 +0200

    changements compte/connexion et accueil

--- Code/website/templates/accueil.php
+++ Code/website/templates/accueil.php
@@ -1,45 +1,59 @@
 <div class="mb-5">
     <div class="background-section mb-3">
+        <div class="h1 text-center mt-2" style="font-weight: bold;">Blabla 2i</div>
+        <br>
+        <div class="subtitle text-center mt-2">Oubliez tous vos soucis, il y a Blabla 2i <br> La première plateforme de covoiturage entre le site de Centrale Lille et le site de l'IG2I à Lens</div>
+        <br>
+        <div class="h4 text-center" style="font-weight: bold;">Trouver le covoiturage parfait n'a jamais été aussi <span id="typing-animation" class="orange-bold-text" style="color: #EA8A2C; font-weight: bold;"> simple.</span> <div>
+
+        <br>
+
+        <div class="mt-4 btn-group position-relative top-100 start-50 translate-middle" role="group" aria-label="radio sens button group">
+            <div class="dropdown ms-2 premier_plan" style="display: inline-block; vertical-align: middle;">
+                <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" id="dropdownTypeTrajet" style="height: 100%; padding: 10px;">
+                    Type de trajet
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownTypeTrajet">
+                    <li><a class="dropdown-item" href="#" onclick="updateTypeTrajet('passager')">Passager</a></li>
+                    <li><a class="dropdown-item" href="#" onclick="updateTypeTrajet('vehicule')">Réserver un véhicule</a></li>
+                </ul>
+            </div>
+
+            <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="on" checked>
+            <label class="btn btn-outline-warning ms-2" for="btnradio1">Villeneuve d'Ascq --> Lens</label>
+
+            <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
+            <label class="btn btn-outline-warning" for="btnradio2">Lens --> Villeneuve d'Ascq</label>
+
+            <div class="dropdown ms-2 premier_plan" style="display: inline-block; vertical-align: middle;">
+                <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" id="dropdownNombrePersonnes" style="height: 100%; padding: 10px;">
+                    Nombre de personnes
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownNombrePersonnes">
+                    <li><a class="dropdown-item" href="#" onclick="updateDropdown(this)">1 personne</a></li>
+                    <li><a class="dropdown-item" href="#" onclick="updateDropdown(this)">2 personnes</a></li>
+                    <li><a class="dropdown-item" href="#" onclick="updateDropdown(this)">3 personnes</a></li>
+                    <li><a class="dropdown-item" href="#" onclick="updateDropdown(this)">4 personnes</a></li>
+                    <li><a class="dropdown-item" href="#" onclick="updateDropdown(this)">5 personnes</a></li>
+                    <li><a class="dropdown-item" href="#" onclick="updateDropdown(this)">6 personnes</a></li>
+                </ul>
+            </div>
+            <input type="date" class="form-control ms-2" style="max-width: 200px;">
+        </div>
+        <br>
+        <button type="submit" class="btn btn-warning btn-lg mt-3 position-relative top-100 start-50 translate-middle">Rechercher</button>
+
         <div class="mt-4 container">
-            <div class="h1 text-center mt-2" style="font-weight: bold;">Blabla 2i</div> <br>
-            <div class="subtitle text-center mt-2"> Oubliez tous vos soucis, il y a Blabla 2i <br> La première plateforme de covoiturage entre le site de Centrale Lille et le site de l'IG2I à Lens</div>
-            <button type="button" class="mt-4 btn btn btn-dark position-relative top-100 start-50 translate-middle">Trouver mon trajet</button>
-
-            <div class="ms-4 h4" style="font-weight: bold;">Economisez du temps, de l’argent...</div> <br>
-            <div class="ms-4 subtitle"> ... Et faites plaisir à la planète ! On estime à 12 % l'économie d'émission de gaz à effet de serre à l'échelle d'une voiture en covoiturage</div>
-            <button type="button" class="ms-4 mt-4 btn btn btn-dark">Trouver mon trajet</button>
+            <div class="ms-4 h4" style="font-weight: bold;">Economisez du temps, de l’argent...</div>
+            <br>
+            <div class="ms-4 subtitle">... Et faites plaisir à la planète ! On estime à 12 % l'économie d'émission de gaz à effet de serre à l'échelle d'une voiture en covoiturage</div>
+            <button type="button" class="ms-4 mt-4 btn btn btn-dark" onclick="window.location.href='index.php?view=trajet'">Créer mon trajet</button>
         </div>
     </div>
-    <div class="h4 text-center" style="font-weight: bold;">Trouver le covoiturage parfait n'a jamais été aussi <span class="orange-bold-text" style="color: #EA8A2C; font-weight: bold;">simple.</span> </div> <br>
-
-    <div class="mt-4 btn-group position-relative top-100 start-50 translate-middle" role="group" aria-label="radio sens button group">
-        <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="on" checked>
-        <label class="btn btn-outline-warning" for="btnradio1">Villeneuve d'Ascq --> Lens</label>
-
-        <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
-        <label class="btn btn-outline-warning" for="btnradio2">Lens --> Villeneuve d'Ascq</label>
-
-        <div class="dropdown ms-2" style="display: inline-block; vertical-align: middle;">
-            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="height: 100%; padding: 10px;">
-                Nombre de personnes
-            </button>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">1 personne</a></li>
-                <li><a class="dropdown-item" href="#">2 personnes</a></li>
-                <li><a class="dropdown-item" href="#">3 personnes</a></li>
-                <li><a class="dropdown-item" href="#">4 personnes</a></li>
-                <li><a class="dropdown-item" href="#">5 personnes</a></li>
-                <li><a class="dropdown-item" href="#">6 personnes</a></li>
-            </ul>
-        </div>
-        <input type="date" class="form-control ms-2" style="max-width: 200px;">
-    </div>
-    <br>
-    <button type="submit" class="btn btn-warning btn-lg mt-3 position-relative top-100 start-50 translate-middle"> Rechercher</button>
 
     <hr>
-    <div class="h4 text-center" style="font-weight: bold;"> Ils parlent de nous </div> <br>
-
+    <div class="h4 text-center" style="font-weight: bold;">Ils parlent de nous</div>
+    <br>
 
     <div class="container mt-2">
         <div class="quotes-container">
@@ -63,3 +77,35 @@
         </div>
     </div>
 </div>
+
+<script>
+    function updateDropdown(element) {
+        document.getElementById("dropdownNombrePersonnes").textContent = element.textContent;
+    }
+
+    function updateTypeTrajet(type) {
+        document.getElementById("dropdownTypeTrajet").textContent = type === 'passager' ? 'Passager' : 'Réserver un véhicule';
+    }
+      // Fonction pour simuler l'effet de texte tapé sur un clavier
+      function typeWriterEffect(textElement, text, speed) {
+          let i = 0;
+          const typingInterval = setInterval(() => {
+              if (i < text.length) {
+                  textElement.textContent += text.charAt(i);
+                  i++;
+              } else {
+                  clearInterval(typingInterval);
+              }
+          }, speed);
+      }
+      
+      // Appel de la fonction lorsque la fenêtre est chargée
+      window.onload = function () {
+          const textElement = document.getElementById('typing-animation');
+          const text = "simple.";
+          const speed = 100; // Vitesse de frappe, ajustez selon votre préférence
+          typeWriterEffect(textElement, text, speed);
+      };
+
+
+</script>
